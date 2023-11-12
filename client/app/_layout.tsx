3f732ba382842dff6b7722763cebb044afa19ab4
James <james.ide775@gmail.com>
2023-11-12 20:28:49 +1030

    title

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -2,13 +2,12 @@ import { Stack } from "expo-router";
 export default function StackLayout() {
   // TODO here set up auth State listener
   return (
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{
-          title: "Login",
-        }}
-      />
+    <Stack
+      screenOptions={{
+        title: "Subscription Tracker",
+      }}
+    >
+      <Stack.Screen name="index" />
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
   );
